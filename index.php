8d2683c00c3c44f777493a9aa3e748b43361ff83
stivex001 <stephenadeyemo@gmail.com>
2023-07-11 23:42:50 +0100

    feat: learnt how to write switch in php

--- index.php
+++ index.php
@@ -1,2 +1,41 @@
 <?php
-echo "Hello World!!!!";
+// echo "Hello World!!!!";
+
+
+// $name = "stephen";
+// $age = 20;
+
+
+
+// echo "$name is $age years old";
+
+// define('NAME', 'stephen');
+// define('AGE', 20);
+
+// echo NAME .  'is'  .  AGE;
+
+// $fruits = array("orange", "apple", "pear");
+
+// echo $fruits[1];
+
+// $age = 27;
+// $time = date('H');
+
+// if ($time < 13) {
+//     echo "Good morning";
+// } else {
+//     echo "Good evening";
+// };
+
+$favColor = "";
+
+switch ($favColor) {
+    case "red":
+        echo 'your favorite color is red';
+        break;
+    case "blue":
+        echo 'your favorite color is blue';
+        break;
+    default:
+        echo 'Your faourite color is white';
+}
